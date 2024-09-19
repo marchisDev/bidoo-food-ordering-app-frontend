e8e670ba9686fb260eb07fe05a420e5a522cf961
marchisDev <buiquangchicv0303@gmail.com>
2024-09-19 16:23:56 +0700

    adding checkout flow

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
 
     return (
         <div className='flex flex-col gap-12'>
-            <div className='md:mx-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
+            <div className='md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
                 <h1 className='text-5xl font-bold tracking-tight text-orange-600'>
                     Truck into a takeaway today!!!
                 </h1>
@@ -25,7 +25,7 @@ export default function HomePage() {
                     <img src={landingImage} />
                     <div className='flex flex-col items-center justify-center gap-4 text-center'>
                         <span className='font-bold text-3xl tracking-tighter'>
-                            Order takeaway evven faster!
+                            Order take away even faster!
                         </span>
                         <span>
                             Download the BiDoo App for faster ordering and personalized
